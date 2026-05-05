5539f39cffb5354e01437f265f10a57b3953ea3c
ma-4578 <mmmusic21999@gmail.com>
2026-05-05 08:08:34 +0630

    Update sudoers.py

--- Newmusic/plugins/sudoers.py
+++ Newmusic/plugins/sudoers.py
@@ -1,7 +1,6 @@
 #Khithlainhtet
 
-
-from pyrogram import filters, types
+from pyrogram import enums, filters, types # enums ကို import ထည့်ထားပါတယ်
 
 from Newmusic import app, db, lang
 from Newmusic.helpers import utils
@@ -12,22 +11,22 @@ from Newmusic.helpers import utils
 async def _sudo(_, m: types.Message):
     user = await utils.extract_user(m)
     if not user:
-        return await m.reply_text(m.lang["user_not_found"])
+        return await m.reply_text(m.lang["user_not_found"], parse_mode=enums.ParseMode.HTML)
 
     if m.command[0] == "addsudo":
         if user.id in app.sudoers:
-            return await m.reply_text(m.lang["sudo_already"].format(user.mention))
+            return await m.reply_text(m.lang["sudo_already"].format(user.mention), parse_mode=enums.ParseMode.HTML)
 
         app.sudoers.add(user.id)
         await db.add_sudo(user.id)
-        await m.reply_text(m.lang["sudo_added"].format(user.mention))
+        await m.reply_text(m.lang["sudo_added"].format(user.mention), parse_mode=enums.ParseMode.HTML)
     else:
         if user.id not in app.sudoers:
-            return await m.reply_text(m.lang["sudo_not"].format(user.mention))
+            return await m.reply_text(m.lang["sudo_not"].format(user.mention), parse_mode=enums.ParseMode.HTML)
 
         app.sudoers.discard(user.id)
         await db.del_sudo(user.id)
-        await m.reply_text(m.lang["sudo_removed"].format(user.mention))
+        await m.reply_text(m.lang["sudo_removed"].format(user.mention), parse_mode=enums.ParseMode.HTML)
 
 
 o_mention = None
@@ -36,7 +35,7 @@ o_mention = None
 @lang.language()
 async def _listsudo(_, m: types.Message):
     global o_mention
-    sent = await m.reply_text(m.lang["sudo_fetching"])
+    sent = await m.reply_text(m.lang["sudo_fetching"], parse_mode=enums.ParseMode.HTML)
 
     if not o_mention:
         o_mention = (await app.get_users(app.owner)).mention
@@ -52,4 +51,5 @@ async def _listsudo(_, m: types.Message):
         except Exception:
             continue
 
-    await sent.edit_text(txt)
+    # edit_text မှာ parse_mode ထည့်ပေးထားလို့ Premium Emoji တွေ ပေါ်ပါလိမ့်မယ်
+    await sent.edit_text(txt, parse_mode=enums.ParseMode.HTML)
